5105024c7bbb0bd7d50ad918a0137364f0a75ab7
Vithusan <vithusan_ak@hotmail.com>
2016-01-31 14:42:56 -0800

    updated #2 Signed-off-by:Vithusan <vithusan_ak@hotmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -34,6 +34,13 @@ class Welcome extends Application {
 		$this->render();
 	}
 
+        function shucks() 
+        {
+            $this->data['pagebody'] = 'justone'; // this is the view we want shown
+            $record = $this->quotes->get("2"); // reference the first method inside quotes.php model
+            $this->data = array_merge($this->data, $record); // merges the second parameter into the first parameter, and returns the result
+            $this->render();
+        }
 }
 
 /* End of file Welcome.php */
