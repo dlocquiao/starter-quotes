960d4a24bbbbd4f441c6aa308562f18b5ed2d398
Vithusan <vithusan_ak@hotmail.com>
2016-01-31 12:33:22 -0800

    completed Signed-off-by:Vithusan <vithusan_ak@hotmail.com>

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -26,5 +26,15 @@ class First extends Application {
 
 		$this->render();
 	}
+        
+        function zzz()
+        {
+            $this->data['pagebody'] = 'justone';	// this is the view we want shown
+		// build the list of authors, to pass on to our view
+		$record = $this->quotes->first();
+		$this->data = array_merge($this->data, $record);
+
+		$this->render();
+        }
 
 }
